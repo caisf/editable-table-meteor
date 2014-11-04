c34784cb7dc5949d0c30a160a28607d2050a504b
Tejas Manohar <me@tejasmanohar.com>
2014-11-04 13:40:52 -0600

    still erroring but different error

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tejasmanohar:editable-table',
   summary: 'tiny jQuery/Bootstrap widget that makes a HTML table editable ',
-  version: '1.0.3',
+  version: '1.0.4',
   git: 'https://github.com/tejas-manohar/editable-table-meteor'
 });
 
