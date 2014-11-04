b5f8259998d81727607fd41c685ad26ad3d94276
Tejas Manohar <me@tejasmanohar.com>
2014-11-04 12:40:52 -0600

    package.js created with minimal details

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'tejas-manohar:editable-table',
+  summary: 'tiny jQuery/Bootstrap widget that makes a HTML table editable ',
+  version: '1.0.0',
+  git: 'https://github.com/tejas-manohar/editable-table-meteor'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+    api.use('jquery@2.1.1');
+    api.addFiles('vendor/mindmup-editabletable.min.js');
+});
