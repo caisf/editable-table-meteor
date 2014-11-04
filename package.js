337048d0bcc32c1f9d8a7f8031dd25bc7687deac
Tejas Manohar <me@tejasmanohar.com>
2014-11-04 12:54:09 -0600

    specify meteor version in correct format

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.0');
+    api.versionsFrom('1.0.0');
     api.use('jquery@1.0');
     api.addFiles('vendor/mindmup-editabletable.min.js');
 });
